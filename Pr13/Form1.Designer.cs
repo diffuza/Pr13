3ccfe6f04f8f1d4cefbcc803693a5e4de15f7a97
USER <USER@DESKTOP-TRQG01D>
2023-03-21 20:01:03 +0300

    Добавьте файлы проекта.

---
+++ Pr13/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Pr13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.grayButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 284);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(528, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(455, 284);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.saveButton.Location = new System.Drawing.Point(637, 303);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(243, 33);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Сохранить изображение";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // grayButton
+            // 
+            this.grayButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grayButton.Location = new System.Drawing.Point(444, 303);
+            this.grayButton.Name = "grayButton";
+            this.grayButton.Size = new System.Drawing.Size(119, 33);
+            this.grayButton.TabIndex = 3;
+            this.grayButton.Text = "Ч\\Б";
+            this.grayButton.UseVisualStyleBackColor = true;
+            this.grayButton.Click += new System.EventHandler(this.grayButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.openButton.Location = new System.Drawing.Point(115, 302);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(202, 34);
+            this.openButton.TabIndex = 4;
+            this.openButton.Text = "Загрузить изображение";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click_1);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 361);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(971, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.grayButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Практическая работа №13";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Button saveButton;
+        private Button grayButton;
+        private Button openButton;
+        private TrackBar trackBar1;
+    }
+}
---
+++ Pr13/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Pr13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null) // если изображение в pictureBox2 имеется
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Сохранить картинку как...";
+                sfd.OverwritePrompt = true; // показывать ли "Перезаписать файл" если пользователь указывает имя файла, который уже существует
+                sfd.CheckPathExists = true; // отображает ли диалоговое окно предупреждение, если пользователь указывает путь, который не существует
+                                            // фильтр форматов файлов
+                sfd.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                sfd.ShowHelp = true; // отображается ли кнопка Справка в диалоговом окне
+                                     // если в диалоге была нажата кнопка ОК
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        // сохраняем изображение
+                        pictureBox2.Image.Save(sfd.FileName);
+                    }
+                    catch // в случае ошибки выводим MessageBox
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }    
+        }
+
+        private void grayButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) // если изображение в pictureBox1 имеется
+            {
+                // создаём Bitmap из изображения, находящегося в pictureBox1
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                // создаём Bitmap для черно-белого изображения
+                Bitmap output = new Bitmap(input.Width, input.Height);
+                // перебираем в циклах все пиксели исходного изображения
+                for (int j = 0; j < input.Height; j++)
+                    for (int i = 0; i < input.Width; i++)
+                    {
+                        // получаем (i, j) пиксель
+                        UInt32 pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
+                        // получаем компоненты цветов пикселя
+                        float R = (float)((pixel & 0x00FF0000) >> 16); // красный
+                        float G = (float)((pixel & 0x0000FF00) >> 8); // зеленый
+                        float B = (float)(pixel & 0x000000FF); // синий
+                                                               // делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                        R = G = B = (R + G + B) / 3.0f;
+                        // собираем новый пиксель по частям (по каналам)
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        // добавляем его в Bitmap нового изображения
+                        output.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                // выводим черно-белый Bitmap в pictureBox2
+                pictureBox2.Image = output;
+            }
+        }
+
+        private void openButton_Click_1(object sender, EventArgs e)
+        {
+            // диалог для выбора файла
+            OpenFileDialog ofd = new OpenFileDialog();
+            // фильтр форматов файлов
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            // если в диалоге была нажата кнопка ОК
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // загружаем изображение
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                }
+                catch // в случае ошибки выводим MessageBox
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox2.Image = ToImage(ToByte(pictureBox1.Image, (byte)trackBar1.Value));
+        }
+        public int[,] ToByte(Image img, byte level = 230)
+        {
+            var bmp = new Bitmap(img);
+            var mass = new int[bmp.Width, bmp.Height];
+            for (var y = 0; y < img.Height; y++)
+            {
+                for (var x = 0; x < img.Width; x++)
+                {
+                    var isWhite = bmp.GetPixel(x, y).R >= level &&
+                                  bmp.GetPixel(x, y).G >= level &&
+                                  bmp.GetPixel(x, y).B >= level;
+                    mass[x, y] = isWhite ? 0 : 1;
+                }
+            }
+            return mass;
+        }
+        public Image ToImage(int[,] img)
+        {
+            var bmp = new Bitmap(img.GetLength(0), img.GetLength(1));
+            for (var y = 0; y < bmp.Height; y++)
+            {
+                for (var x = 0; x < bmp.Width; x++)
+                {
+                    bmp.SetPixel(x, y, img[x, y] == 1 ? Color.Black : Color.White);
+                }
+            }
+            return bmp;
+        }
+    }
+}
+
+
+        
+
+       
